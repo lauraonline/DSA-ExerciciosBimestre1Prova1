9534fcf5cccbed32e379da620bf7cef2a5ffc5aa
Laura Alves <lauraalves.contato@gmail.com>
2025-08-26 17:37:07 -0300

    Finish vetores2

---
+++ Vetores2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+const int vetN = 4;
+typedef float tvetor[vetN];
+
+float verificarMaiorIdade(tvetor vetor) {
+    float maior = vetor[0];
+    for (int i = 0; i < vetN; i++) {
+        if (vetor[i] > maior) {
+            maior = vetor[i];
+        }
+    }
+    return maior;
+}
+
+void popularVetor(tvetor &vetor) {
+    for (int i = 0; i < vetN; i++) {
+        cout<<"Idade da pessoa "<<i+1<<": ";
+        cin>>vetor[i];
+    }
+}
+
+main() {
+    tvetor vetor;
+    popularVetor(vetor);
+    cout<<"Maior idade: "<<verificarMaiorIdade(vetor);
+}
