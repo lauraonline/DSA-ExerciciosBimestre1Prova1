d4147f1a4c9dced86eb41c082919da07f595e4c6
Laura Alves <lauraalves.contato@gmail.com>
2025-08-26 17:44:15 -0300

    Add vetores3 WIP

---
+++ Vetores3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+const int vetN = 4;
+typedef int tvetor[vetN];
+
+tvetor calcularValoresAcimaDaMedia
+
+void popularVetor(tvetor &vetor) {
+    for (int i = 0; i < vetN; i++) {
+        cout<<"Valor "<<i+1<<": ";
+        cin>>vetor[i];
+    }
+}
+
+float calcularMediaVetor(tvetor vetor) {
+    int total = 0;
+    for (int i = 0; i < vetN; i++) {
+        total += vetor[i];
+    }
+    float media = (float)total/vetN;
+    return media;
+}
+
+main() {
+    tvetor vetor;
+    popularVetor(vetor);
+    cout<<"Maior idade: "<<verificarMaiorIdade(vetor);
+}
