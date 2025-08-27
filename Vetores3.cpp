f640eeb869353597312a7908c72022c1ead6456e
Laura Alves <lauraalves.contato@gmail.com>
2025-08-27 11:37:09 -0300

    Try to finish vetores3 WIP

--- Vetores3.cpp
+++ Vetores3.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-const int vetN = 4;
+const int vetN = 5;
 typedef int tvetor[vetN];
 
 void popularVetor(tvetor &vetor) {
@@ -21,8 +21,39 @@ float calcularMediaVetor(tvetor vetor) {
     return media;
 }
 
-main() {
+int calcularNumeroDeValoresMaioresQueAMedia(float media, tvetor vetor) {
+    int resultadoFinal = 0;
+    for (int i = 0; i < vetN;  i++) {
+        if (vetor[i] > media) {
+            resultadoFinal++;
+        }
+    }
+    return resultadoFinal;
+}
+
+void popularVetorComValoresMaioresQueAMedia(float media, tvetor vetor, tvetor vetorFinal) {
+    for (int i, j = 0; i < vetN; i++) {
+        if (vetor[i] > media) {
+            vetorFinal[j] = vetor[i];
+            j++;
+        }
+    }
+}
+
+void exibirVetor(int tamanhoVetor, tvetor vetor) {
+    cout<<"Vetor:"<<endl;
+    for (int i = 0; i < tamanhoVetor; i++) {
+        cout<<vetor[i]<<"\t";
+    }
+}
+
+int main() {
     tvetor vetor;
     popularVetor(vetor);
-    cout<<"Maior idade: "<<verificarMaiorIdade(vetor);
+    float media = calcularMediaVetor(vetor);
+    int tamanhoVetorFinal = calcularNumeroDeValoresMaioresQueAMedia(media, vetor);
+    int vetorFinal[tamanhoVetorFinal];
+    popularVetorComValoresMaioresQueAMedia(media, vetor, vetorFinal);
+    exibirVetor(tamanhoVetorFinal, vetorFinal);
+    return 0;
 }
