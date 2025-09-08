5f8fb82c62bfb065f87d1f1f5432d128c2c19302
Laura Alves <lauraalves.contato@gmail.com>
2025-09-08 17:25:31 -0300

    change Matrizes8 WIP note: hard

--- Matrizes8.cpp
+++ Matrizes8.cpp
@@ -5,10 +5,11 @@ using namespace std;
 const int matI = 10;
 const int matJ = 10;
 typedef float tmatriz[matI][matJ];
+typedef int tvetorint4P[4];
 
-void preencherMatriz(tmatriz &matriz) {
-    for (int i = 0; i < matI; i++) {
-        for (int j = 0; j < matJ; j++) {
+void preencherMatriz(tmatriz &matriz, int matIatual, int matJatual) {
+    for (int i = 0; i < matIatual; i++) {
+        for (int j = 0; j < matJatual; j++) {
             cout<<"["<<i<<"]["<<j<<"]: ";
             cin>>matriz[i][j];
         }
@@ -37,19 +38,32 @@ bool verificarPossibilidadeProduto (int j1, int i2) {
     return possivel;
 }
 
+void multiplicarMatriz (tvetorint4P ordens, tmatriz matriz1, tmatriz matriz2, tmatriz &matriz3) {
+    for (int i = 0; )
+}
+
 main() {
     tmatriz matriz1;
     tmatriz matriz2;
-    int matriz1I, matriz1J, matriz2I, matriz2J;
+    tvetorint4P ordemMatrizes;
 
     cout<<"Ordem 1 matriz 1: ";
-    receberOrdem(matriz1I);
+    receberOrdem(ordemMatrizes[0]);
     cout<<"Ordem 2 matriz 1: ";
-    receberOrdem(matriz1J);
+    receberOrdem(ordemMatrizes[1]);
     cout<<"Ordem 1 matriz 2: ";
-    receberOrdem(matriz2I);
+    receberOrdem(ordemMatrizes[2]);
     cout<<"Ordem 2 matriz 2: ";
-    receberOrdem(matriz2J);
+    receberOrdem(ordemMatrizes[3]);
+    
+    if (verificarPossibilidadeProduto(matriz1J, matriz2I)) {
+        cout<<"Matriz 1:"<<endl;
+        preencherMatriz(matriz1, matriz1I, matriz1J);
+        cout<<"Matriz 2:"<<endl;
+        preencherMatriz(matriz2, matriz2I, matriz2J);
+    } else {
+        cout<<"Produto impossível! "<<matriz1J<<" =/= "<<matriz2I<<endl;
+    }
 
-    cout<<"Possivel: "<<verificarPossibilidadeProduto(matriz1J, matriz2I);
+    
 }
